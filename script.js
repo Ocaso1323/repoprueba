592bbf70a7890c1fd854a8f04aa5e94b745ddcb3
adriana paredes <128195602+Ocaso1323@users.noreply.github.com>
2023-05-25 17:51:35 -0500

    Add files via upload
    pasapalabras

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
-const imgs = document.getElementById('imgs')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
+// Total de preguntas del juego
+const TOTAL_PREGUNTAS = 10;
+//variables para controlar el tiempo
+const timer = document.getElementById("tiempo");
+//tiempo del juego en segundos
+const TIEMPO_DEL_JUEGO = 60;
+//variable que indica el tiempo restante
+let tiempoRestante = TIEMPO_DEL_JUEGO;
+//variable que maneja el contador
+var countdown;
 
-const img = document.querySelectorAll('#imgs img')
+// creamos las letras de la A a la J de forma circular
 
-let idx = 0
+const container = document.querySelector(".container");
+for (let i=1; i <= TOTAL_PREGUNTAS; i++){
+    const circle = document.createElement("div");
+    circle.classList.add("circle");
+    circle.textContent = String.fromCharCode(i + 96);
+    circle.id = String.fromCharCode(i + 96);
+    container.appendChild(circle);
 
-let interval = setInterval(run, 2000)
-
-function run() {
-    idx++
-    changeImage()
+    const angle = ((i - 1) / TOTAL_PREGUNTAS) * Math.PI * 2 - (Math.PI / 2);
+    const x = Math.round(95 + 120 * Math.cos(angle));
+    const y = Math.round(95 + 120 * Math.sin(angle));
+    circle.style.left = `${x}px`;
+    circle.style.top = `${y}px`;
 }
 
-function changeImage() {
-    if(idx > img.length - 1) {
-        idx = 0
-    } else if(idx < 0) {
-        idx = img.length - 1
-    }
+//boton comenzar
 
-    imgs.style.transform = `translateX(${-idx * 500}px)`
-}
+let comenzar = document.getElementById("comenzar");
+comenzar.addEventListener("click", function(event) {
+    document.getElementById("pantalla-inicial").style.display = "none";
+    document.getElementById("pantalla-juego").style.display ="block";
 
-function resetInterval() {
-    clearInterval(interval)
-    interval = setInterval(run, 2000)
-}
-
-rightBtn.addEventListener('click', () => {
-    idx++
-    changeImage()
-    resetInterval()
+    //largamos el tiempo
+    iniciarContador();
 })
 
-leftBtn.addEventListener('click', () => {
-    idx--
-    changeImage()
-    resetInterval()
-})
+function iniciarContador() {
+    countdown = setInterval(()=>{
+
+        //restar un segundo al tiempo restante
+        tiempoRestante--;
+
+        //actualizamos el texto del cronometro con el tiempo restante
+        timer.innerText = tiempoRestante;
+
+        //si el tiempo llega a 0, detener el cronometro
+        if(tiempoRestante<0){
+        clearInterval(countdown);
+        alert("se acabo el tiempo");
+        //mostrar pantalla final();
+        
+
+        }
+    }, 1000);
+}
+ 
+
